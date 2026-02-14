61b31767576af419d5926540b2d2ca0f30763fba
mohammedbabangida94-bit <mohammedbabangida94@gmail.com>
2026-02-14 19:44:02 +0100

    Initial boilerplate VigilantNG:Estate Security Solutions

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'vigilantNG-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+  // Add any other CSS or JS files you have here, e.g., '/style.css'
+];
+
+// 1. Install Event: Save files to the "bank" (cache)
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// 2. Fetch Event: Intercept requests and serve from the "bank" if offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
